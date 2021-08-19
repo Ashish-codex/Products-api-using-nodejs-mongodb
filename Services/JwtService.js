fe0a39a28afa903164281c38d85085fa7614fc44
Ashish-codex <ashish.prajapati16200@gmail.com>
2021-08-19 12:39:41 +0530

    changed

--- Services/JwtService.js
+++ Services/JwtService.js
@@ -14,7 +14,7 @@ class JwtService {
 
 
     // For Refresh token usage
-    static signRefreshToken(payload, secret = JWT_REFRESH_SECRET, expiry = '1y'){
+    static signRefreshToken(payload, secret = JWT_REFRESH_SECRET, expiry = '24h'){
         return jwt.sign(payload, secret, { expiresIn: expiry })
     }
 
